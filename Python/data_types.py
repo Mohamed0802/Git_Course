0bf16179053041b910ee36e26efc47d53a0aec1a
Mohamed Ali Hmida <medali.hmida12@gmail.com>
2024-08-18 20:40:14 +0200

    handling the data types of python

---
+++ Python/data_types.py
@@ -0,0 +1,52 @@
+#string data type
+
+first= 'Mohamed Ali'
+last='Hmida'
+
+# # print(type(first))
+# # print(type(first))
+# # print(type(first)==str) 
+
+# #constructor funktion
+# pizza=str("pepperoni")
+# print(type(pizza))
+# print(type(pizza)==str)
+# print(type(pizza)==str) 
+
+# #Concatenation
+# fullname=first+" "+last
+# print(fullname)
+
+# fullname+="!"
+# print(fullname)
+
+# #casting a number to a string
+# decade=str(2002)
+# print(type(decade))
+# print(decade)
+
+# statement="i like the rap musik from the "+decade+" great"
+# print (statement)
+
+# #multiple lines
+multiline='''
+hey, how are you
+
+ i was just checking in
+                              all good
+
+  '''
+# print (multiline)
+
+
+#Mohamed Ali
+print(first[4])
+print(first[1:4])
+print(first.startswith("M"))
+print(first.endswith("o"))
+print(type(first    ))
+
+gpa =3.8
+import math
+print(math.pi)
+p  
